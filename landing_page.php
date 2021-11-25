cb0043409e65c1e063bc09cd6c8e14ad5a0f7527
Rhodalynn <rhodalyn.nettey@ashesi.edu.gh>
2021-11-25 20:36:50 +0000

    Update landing_page.php

--- landing_page.php
+++ landing_page.php
@@ -5,8 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ash Gym | LandingPage</title>
+    <link rel="stylesheet" href="Homepage.css">
+
 </head>
 <body>
-    
+<!-- Top bar -->
+        <div class="top_navbar">
+            <!-- logo -->
+                <div class="logo">Ash Gym</div>
+                    <ul>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    </ul>
+        
+                 </div>
+  <div>
+    <h1>WELCOME TO ASH GYM</h1>
+  </div>
+
+  <div class="buttons">
+      <ul>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
+      </ul>
+</div>
+
+   
 </body>
 </html>
