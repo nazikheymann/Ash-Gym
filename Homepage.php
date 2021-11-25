4d2eeb20dd7cd71b5746f2de3845f0bac0a6a160
nazikheymann <94528637+nazikheymann@users.noreply.github.com>
2021-11-25 16:24:35 +0000

    Add files via upload

---
+++ Homepage.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src= https://kit.fontawesome.com/b99e675b6e.js></script>
+    <title>Ash Gym | Home</title>
+    <link rel="stylesheet" href="Homepage.css">
+</head>
+<body>
+    <!-- <div class="wrapper hover_collapse"> -->
+
+        <!-- Top bar -->
+        <div class="top_navbar">
+            <!-- logo -->
+                <div class="logo">Ash Gym</div>
+            <!-- menu button -->
+
+            <div id="main">
+
+                <!-- <div class="menu"> -->
+                    
+                  <!-- </div> -->
+
+                <button class="openbtn" onclick="openNav()">
+                <div class="hamburger">
+                    <i class="fas fa-bars"></i>
+                    </div>
+                
+                </button>
+    
+    
+            </div>
+
+               <!-- <div class="menu">
+                 <div class="hamburger">
+                 <i class="fas fa-bars"></i>
+                 </div>
+               </div> -->
+            
+            <!-- </div> -->
+        <!-- Sidebar -->
+        <div id = "mysidebar" class="sidebar">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+            <div class="sidebar_inner">
+                <ul>
+                    <li>
+                    <a href="gym_demo.html">
+                  <span class="icon"><i class="fa fa-home"></i></span>
+                  <span class="text">Home</span>
+                    </a>
+                    </li>
+              
+                    <li>
+                    <a href="equipment_demo.html">
+                  <span class="icon"><i class="fa fa-dumbbell"></i></span>
+                  <span class="text">Equipment</span>
+                    </a>
+                    </li>
+                    <li>
+                    <a href="members_demo.html">
+                  <span class="icon"><i class="fa fa-users"></i></span>
+                  <span class="text">Members</span>
+                    </a>
+                    </li>
+
+                </ul>
+              </div>
+        </div>
+        
+
+
+        <script>
+            function openNav() {
+              document.getElementById("mysidebar").style.width = "250px";
+              document.getElementById("main").style.marginLeft = "250px";
+            }
+            
+            function closeNav() {
+              document.getElementById("mysidebar").style.width = "0";
+              document.getElementById("main").style.marginLeft= "0";
+            }
+            </script>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="gym_style.css">
+    <title>Ash Gym | Sign up</title>
+</head>
+<body>
+    <div class="bg-img">
+        <div class="content">
+           <header>Welcome to Ash Gym</header>
+           <form action="#">
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="Name">
+             </div><br>
+              <div class="field">
+                 <span class="fa fa-user"></span>
+                 <input type="text" required placeholder="Email">
+              </div>
+              <div class="field space">
+                 <span class="fa fa-lock"></span>
+                 <input type="password" class="pass-key" required placeholder="Password">
+              </div><br>
+              <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="Confirm Password">
+             </div>
+              <div class="pass">
+                 <a href="#">Forgot Password?</a>
+              </div>
+
+              <div class="field">
+                 <input type="submit" value="SIGN UP">
+              </div>
+           </form>
+           <div class="login">
+              Already have an account?
+              <a href="login.php">Login</a>
+           </div>
+</body>
+</html>
