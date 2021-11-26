20eee24179e299605ed8955d4373582bbc410f63
naalamilex <76079227+naalamilex@users.noreply.github.com>
2021-11-26 02:28:12 +0000

    Create update.php

---
+++ test/update.php
@@ -0,0 +1,174 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$fname = $lname = $email = "";
+$fname_err = $lname_err = $email_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["client_id"]) && !empty($_POST["client_id"])){
+    // Get hidden input value
+    $id = $_POST["client_id"];
+    
+    // Validate first name
+    $input_fname = trim($_POST["fname"]);
+    if(empty($input_fname)){
+        $fname_err = "Please enter your first name.";
+    } elseif(!filter_var($input_fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $fname_err = "Please enter a valid name.";
+    } else{
+        $fname = $input_fname;
+    }
+    
+  // Validate last name
+  $input_lname = trim($_POST["lname"]);
+  if(empty($input_lname)){
+      $lname_err = "Please enter your last name.";
+  } elseif(!filter_var($input_lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+      $lname_err = "Please enter a valid name.";
+  } else{
+      $lname = $input_lname;
+  }
+    
+    // Validate email 
+    $input_email = trim($_POST["email"]);
+    if(empty($input_email)){
+        $email_err = "Please enter your email.";
+    } elseif(!filter_var($input_lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $email_err = "Please enter a valid email address.";
+    } else{
+        $email= $input_email;
+    }
+    
+    
+    
+    // Check input errors before inserting in database
+    if(empty($fname_err) && empty($lname_err) && empty($email_err)){
+        // Prepare an update statement
+        $sql = "UPDATE Client SET fname=?, lname=?, email=? WHERE client_id=?";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sssi", $param_fname, $param_lname, $param_email, $param_client_id);
+            
+            // Set parameters
+            $param_fname = $fname;
+            $param_lname = $lname;
+            $param_email = $email;
+            $param_client_id = $client_id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["client_id"]) && !empty(trim($_GET["client_id"]))){
+        // Get URL parameter
+        $client_id =  trim($_GET["client_id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM Client WHERE client_id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_client_id);
+            
+            // Set parameters
+            $param_client_id = $client_id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $fname = $row["fname"];
+                    $lname = $row["lname"];
+                    $email = $row["email"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($link);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Update Record</h2>
+                    <p>Please edit the input values and submit to update the client record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group">
+                            <label>First Name</label>
+                            <input type="text" name="fname" class="form-control <?php echo (!empty($fname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $fname; ?>">
+                            <span class="invalid-feedback"><?php echo $fname_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name</label>
+                            <textarea name="lname" class="form-control <?php echo (!empty($lname_err)) ? 'is-invalid' : ''; ?>"><?php echo $lname; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $lname_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                            <span class="invalid-feedback"><?php echo $email_err;?></span>
+                        </div>
+                        <input type="hidden" name="client_id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="../final-group-project/members_demo.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
